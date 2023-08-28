68cd636f615cd71105d4553280aefa1726d1861d
Yasin Akdoğan <yasin_akdogan15@hotmail.com>
2023-08-28 03:07:01 +0300

    Splash Activity Finished.

--- app/src/main/java/com/example/twitterapplication/activity_splash.java
+++ app/src/main/java/com/example/twitterapplication/activity_splash.java
@@ -2,13 +2,25 @@ package com.example.twitterapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class activity_splash extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        toRegister();
+    }
+    private void toRegister() {
+        int delay = 3000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(activity_splash.this, FirstRegisterActivity.class));
+                finish();
+            }
+        },delay);
     }
 }
